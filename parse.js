eafe0cef76797eff76aba23db051597c88379581
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-08-16 22:15:25 -0400

    Update filter

--- parse.js
+++ parse.js
@@ -2,18 +2,41 @@
 
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
-import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11/mod.ts' 
-// const fs = require('fs');
+// import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11/mod.ts' 
+const fs = require('fs');
 // Step 1: Read the downloaded_filename JSON
-const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
-const json = await readJSON(filename)
+// const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
+const filename = 'ja3er.json';
+// const json = await readJSON(filename)
 
 
-// let rawdata = fs.readFileSync('ja3er.json');
-// let student = JSON.parse(rawdata);
-const json_obj = Object.values(json)
-const filtered_array = json_obj.filter(d => d.Count > 10);
-// console.log(arr1)
+let token_data = fs.readFileSync('ja3tokens.json');
+let filtered_token = JSON.parse(token_data);
+
+
+let user_agent = fs.readFileSync('ja3er-price-postprocessed.json');
+
+let filtered_user_agent = JSON.parse(user_agent);
+// const json_obj = Object.values(json)
+
+filtered_user_agent = filtered_user_agent.filter(d => d.Count > 10);
+filtered_user_agent = filtered_user_agent.filter(d => d["User-Agent"] !== '');
+
+// let combino = []
+// for (let a of filtered_token) {
+//     const md5 = a.md5
+//     for (let b of filtered_user_agent) {
+//         const md52 = b.md5
+//         if (md5 === md52) {
+//             combino.push({md5: md5, ja3: a.ja3, "User-Agent": b["User-Agent"]})
+//             break;
+//         }
+        
+//     }
+// }
+
+
+console.log(addedMessageGroupIds)
 // // Step 2: Filter specific data we want to keep and write to a new JSON file
 // const currencyRates = Object.values(json.bpi); // convert property values into an array
 // const filteredCurrencyRates = currencyRates.map(rate => ({ 
@@ -22,9 +45,9 @@ const filtered_array = json_obj.filter(d => d.Count > 10);
 // }));
 
 // Step 3. Write a new JSON file with our filtered data
-const newFilename = `ja3er-price-postprocessed.json` // name of a new file to be saved
-await writeJSON(newFilename, filtered_array) // create a new JSON file with just the Bitcoin price
-console.log("Wrote a post process file")
+// const newFilename = `ja3er-price-postprocessed.json` // name of a new file to be saved
+// await writeJSON(newFilename, filtered_array) // create a new JSON file with just the Bitcoin price
+// console.log("Wrote a post process file")
 
-// Optionally delete the original file
-await removeFile('./ja3er.json') // equivalent to removeFile('btc-price.json')
+// // Optionally delete the original file
+// await removeFile('./ja3er.json') // equivalent to removeFile('btc-price.json')
--- postprocess.js
+++ postprocess.js
@@ -5,21 +5,50 @@
 import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11/mod.ts' 
 
 // Step 1: Read the downloaded_filename JSON
-const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
-const json = await readJSON(filename)
-console.log(json)
+// const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
+const user_agent = 'user_agent.json'
+const token_data = 'ja3tokens.json';
 
-// Step 2: Filter specific data we want to keep and write to a new JSON file
-const currencyRates = Object.values(json.bpi); // convert property values into an array
-const filteredCurrencyRates = currencyRates.map(rate => ({ 
-    currency: rate.description,
-    bitcoinRate: rate.rate
-}));
+
+// let token_data = fs.readFileSync('ja3tokens.json');
+let filtered_token = await readJSON(token_data)
+filtered_token = Object.values(filtered_token)
+
+// let user_agent = fs.readFileSync('ja3er-price-postprocessed.json');
+
+let filtered_user_agent = await readJSON(user_agent)
+filtered_user_agent = Object.values(filtered_user_agent)
+// const json_obj = Object.values(json)
+
+filtered_user_agent = filtered_user_agent.filter(d => d.Count > 10);
+filtered_user_agent = filtered_user_agent.filter(d => d["User-Agent"] !== '');
+
+let combino = []
+for (let a of filtered_token) {
+    const md5 = a.md5
+    for (let b of filtered_user_agent) {
+        const md52 = b.md5
+        if (md5 === md52) {
+            combino.push({md5: md5, ja3: a.ja3, "User-Agent": b["User-Agent"]})
+            break;
+        }
+        
+    }
+}
+
+
+// // Step 2: Filter specific data we want to keep and write to a new JSON file
+// const currencyRates = Object.values(json.bpi); // convert property values into an array
+// const filteredCurrencyRates = currencyRates.map(rate => ({ 
+//     currency: rate.description,
+//     bitcoinRate: rate.rate
+// }));
 
 // Step 3. Write a new JSON file with our filtered data
-const newFilename = `btc-price-postprocessed.json` // name of a new file to be saved
-await writeJSON(newFilename, filteredCurrencyRates) // create a new JSON file with just the Bitcoin price
+const newFilename = `ja3er-price-postprocessed.json` // name of a new file to be saved
+await writeJSON(newFilename, combino) // create a new JSON file with just the Bitcoin price
 console.log("Wrote a post process file")
 
-// Optionally delete the original file
-await removeFile('./btc-price.json') // equivalent to removeFile('btc-price.json')
+// // Optionally delete the original file
+await removeFile('./ja3er.json') // equivalent to removeFile('btc-price.json')
+await removeFile('./user_agent.json')
